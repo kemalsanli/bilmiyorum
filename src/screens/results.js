11eee55ced950b3e90850b541a9e9c4a85176d4d
hasimdelil <hasimdelil@gmail.com>
2021-01-04 02:14:36 +0300

    results: edit.

--- src/screens/results.js
+++ src/screens/results.js
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import {Text, StyleSheet, Button,View,FlatList} from 'react-native'
+import React, { useState } from 'react';
+import { Text, StyleSheet, Button, View, FlatList } from 'react-native'
 
-const results=() => {
-    return <View>
-        <Text>results</Text>
-        <StatusBar style="auto"/>
-
-    </View>
-  
+const results = () => {
+  return <View>
+    <Text>results</Text>
+    <StatusBar style="auto"/>
+  </View>
 };
 
-const styles = StyleSheet.create({});
-
+const styles = StyleSheet.create({
+});
 
 export default results;
