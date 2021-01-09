cf115bf2611034ebb4dfdc7ac55a18900225010d
hasimdelil <hasimdelil@gmail.com>
2021-01-09 04:53:27 +0300

    results eklendi.

---
+++ ReactNative/src/screens/results.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Text, StyleSheet, Button, View, FlatList } from 'react-native'
+import HeaderComponent from '../components/HeaderComponent'
+
+const results = () => {
+  return <View>
+    <Text>results</Text>
+  </View>
+};
+
+const styles = StyleSheet.create({
+});
+
+export default results;
